e6f8a6b7af11b21e233e5a133c5f8988241432d5
lanreogunmola@github.com <lanreogunmola@gmail.com>
2016-08-30 17:52:33 -0700

    end of task 15, basic integration with flickr

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,9 +1,19 @@
 import Ember from 'ember';
 var Photo = Ember.Object.extend({
-	   title: '',
-	   username: '',
-	   url: '',
-
+	    title: '',
+	username: '',
+	//flickr extra data
+	owner: '',
+	//flickr url data
+	id: '',
+	farm: 0,
+	secret: '',
+	server: '',
+	url: function(){
+		return "https://farm"+this.get('farm')+
+		".staticflickr.com/"+this.get('server')+
+		"/"+this.get('id')+"_"+this.get('secret')+"_b.jpg";
+	}.property('farm','server','id','secret'),
 });
 
 var PhotoCollection = Ember.ArrayProxy.extend(Ember.SortableMixin, {
@@ -45,8 +55,47 @@ testPhotos.pushObject(testimg4);
 export default Ember.Controller.extend({
 	    photos: testPhotos,
 	    searchField: '',
-		filteredPhotos: testPhotos,
+		filteredPhotos: function () {
+		var filter = this.get('searchField');
+		var rx = new RegExp(filter, 'gi');
+		var photos = this.get('photos');
+
+		return photos.filter(function(photo){
+			return photo.get('title').match(rx) || photo.get('username').match(rx);
+		});
+	}.property('photos.@each','searchField'),
+	actions: {
+		search: function () {
+			this.get('filteredPhotos');
+		}
+	},
 		actions: {
+			getPhotos: function(){
+				var apiKey = 'f73a4f9b7486132776d620f10d4ccf93';
+				var host = 'https://api.flickr.com/services/rest/';
+				var method = "flickr.tags.getClusterPhotos";
+				var tag = "hi";
+				var requestURL = host + "?method="+method + "&api_key="+apiKey+"&tag="+tag+"&format=json&nojsoncallback=1";
+				var photos = this.get('photos');
+				Ember.$.getJSON(requestURL, function(data){
+					//callback for successfully completed requests
+					console.log(data);
+					data.photos.photo.map(function(photo) {
+						var newPhotoItem = Photo.create({
+							title: photo.title,
+							username: photo.username,
+							//flickr extra data
+							owner: photo.owner,
+							//flickr url data
+							id: photo.id,
+							farm: photo.farm,
+							secret: photo.secret,
+							server: photo.server,
+						});
+						photos.pushObject(newPhotoItem);
+					})
+				});
+	},
 			search: function () {
 				var filter = this.get('searchField');
 				var rx = new RegExp(filter, 'gi');
@@ -57,6 +106,7 @@ export default Ember.Controller.extend({
 					})
 				);
 			}
-		}
 
+		}
+             
 });
